2355f31e8b06a6e71f49b0d3b4dc2ab29c883354
Justin Johnston <justin.r.johnston@hotmail.com>
2018-06-24 19:33:25 +0800

    Changed file heirachy, build.bat only copies relevant files

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 echo DA | xcopy /E "%~dp0\dependencies\OIS-1-4\bin" "%~dp0\bin"
-echo DA | xcopy /E "%~dp0\src\Engine\Engine\bin" "%~dp0\bin"
-echo DA | xcopy /E "%~dp0\src\Engine\Core\bin" "%~dp0\bin"
+echo DA | xcopy /E "%~dp0\engine\Engine\bin\*.exe" "%~dp0\bin"
+echo DA | xcopy /E "%~dp0\engine\Core\bin\*dll" "%~dp0\bin"
