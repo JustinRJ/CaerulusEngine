622001bdc1c88810f231276c7e72dbdb50d2addb
Justin Johnston <justin.r.johnston@hotmail.com>
2018-06-24 20:02:12 +0800

    Added SDL2 to Engine project

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
+echo DA | xcopy /E "%~dp0\dependencies\SDL2-2.0.8\bin" "%~dp0\bin"
 echo DA | xcopy /E "%~dp0\dependencies\OIS-1-4\bin" "%~dp0\bin"
+
 echo DA | xcopy /E "%~dp0\engine\Engine\bin\*.exe" "%~dp0\bin"
 echo DA | xcopy /E "%~dp0\engine\Core\bin\*dll" "%~dp0\bin"
