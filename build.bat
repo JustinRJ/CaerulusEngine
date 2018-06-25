ebf724b52ae0be208b74009e635f4c0261b0e04a
Justin Johnston <justin.r.johnston@hotmail.com>
2018-06-25 22:55:34 +0800

    Created Graphics Project

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
-echo DA | xcopy /E "%~dp0dependencies\SDL2-2.0.8\bin" "%~dp0\bin"
-echo DA | xcopy /E "%~dp0dependencies\OIS-1-4\bin" "%~dp0\bin"
+echo DA | xcopy /E "%~dp0dependencies\SDL2-2.0.8\bin"   "%~dp0\bin"
+echo DA | xcopy /E "%~dp0dependencies\OIS-1-4\bin"      "%~dp0\bin"
 
-echo DA | xcopy /E "%~dp0engine\Engine\bin\*.exe" "%~dp0\bin"
-echo DA | xcopy /E "%~dp0engine\Core\bin\*dll" "%~dp0\bin"
+echo DA | xcopy /E "%~dp0engine\Engine\bin\*.exe"       "%~dp0\bin"
+echo DA | xcopy /E "%~dp0engine\Core\bin\*dll"          "%~dp0\bin"
+echo DA | xcopy /E "%~dp0engine\Graphics\bin\*dll"      "%~dp0\bin"
