7cbec1d5d608987ebedbfebfaa5639a83736d6da
Justin Johnston <justin.r.johnston@hotmail.com>
2018-06-25 22:44:25 +0800

    Changed build paths, deleted redundant dlls

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-echo DA | xcopy /E "%~dp0\dependencies\SDL2-2.0.8\bin" "%~dp0\bin"
-echo DA | xcopy /E "%~dp0\dependencies\OIS-1-4\bin" "%~dp0\bin"
+echo DA | xcopy /E "%~dp0dependencies\SDL2-2.0.8\bin" "%~dp0\bin"
+echo DA | xcopy /E "%~dp0dependencies\OIS-1-4\bin" "%~dp0\bin"
 
-echo DA | xcopy /E "%~dp0\engine\Engine\bin\*.exe" "%~dp0\bin"
-echo DA | xcopy /E "%~dp0\engine\Core\bin\*dll" "%~dp0\bin"
+echo DA | xcopy /E "%~dp0engine\Engine\bin\*.exe" "%~dp0\bin"
+echo DA | xcopy /E "%~dp0engine\Core\bin\*dll" "%~dp0\bin"
