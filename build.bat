0199623b7df7e1084f25c38ba67d4fcefb539f30
Justin Johnston <justin.r.johnston@hotmail.com>
2018-07-11 20:59:08 +0800

    Changed binary naming convention

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 echo DA | xcopy /E "%~dp0dependencies\OIS-1-4\bin\%1\%2\OIS.dll"      "%~dp0\bin\%1\%2"
-echo DA | xcopy /E "%~dp0engine\Engine\bin\%1\%2\Engine.exe"          "%~dp0\bin\%1\%2"
-echo DA | xcopy /E "%~dp0engine\Core\bin\%1\%2\Core.dll"              "%~dp0\bin\%1\%2"
-echo DA | xcopy /E "%~dp0engine\Graphics\bin\%1\%2\Graphics.dll"      "%~dp0\bin\%1\%2"
+
+echo DA | xcopy /E "%~dp0engine\Engine\bin\%1\%2\Caerulus_Engine.exe"           "%~dp0\bin\%1\%2"
+echo DA | xcopy /E "%~dp0engine\Core\bin\%1\%2\Caerulus_Core.dll"               "%~dp0\bin\%1\%2"
+echo DA | xcopy /E "%~dp0engine\Graphics\bin\%1\%2\Caerulus_Graphics.dll"       "%~dp0\bin\%1\%2"
